52efb964eb2399dc55f79c637e8af2e7a1e887c6
RAHUL SINGH <rahulsingh14jan95@gmail.com>
2024-01-24 21:58:48 +0530

    Update peer_sender_marsRover.py

--- peer_sender_marsRover.py
+++ peer_sender_marsRover.py
@@ -161,7 +161,7 @@ def main(peer_ip_ports, peer_id):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send data to a peer.")
-    parser.add_argument("--peer_ip_ports", nargs='+', default=[('10.35.70.42:33335'), ('10.35.70.31:33334')], help="List of tuples containing peer IP addresses and ports")
+    parser.add_argument("--peer_ip_ports", nargs='+', default=[('IP_LANDER_MODULE:PORT_LANDER_MODULE'), ('IP_MARS_SATELLITE:PORT_MARS_SATELLITE')], help="List of tuples containing peer IP addresses and ports")
     parser.add_argument("--peer_id", default='Mars_Rover', help="Unique identifier for this peer")
     args = parser.parse_args()
 
